9a3bbe19aa0be6c701c2c189ca49dadd6c4c9372
Vruddhi <toliavruddhi@gmail.com>
2022-06-04 23:12:51 +0530

    main file updated!

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const APP_ID='019d3ffc61184149a23afd37f8a35aa0'
 const TOKEN='006019d3ffc61184149a23afd37f8a35aa0IAA+QhZksZpOgpjPzNxaSTlZyJ/NLSMZLt/chCglKf++vmTNKL8AAAAAEABdi2YtFeScYgEAAQAU5Jxi'
 const CHANNEL='main'
+const client =AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
+
+let localTracks=[]
+let remoteUsers={}
+let joinAndDisplayLocalStream=async()=>{
+    let UID=await client.join(APP_ID,CHANNEL,TOKEN,null)
+    localTracks =await 
+}
